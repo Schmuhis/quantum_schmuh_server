32f0c59e40dfdbd45052904f855ff60f3c1cb3cf
mschoe <mathias.schoettle@oth-regensburg.de>
2024-05-25 10:38:02 +0200

    adapted test controller

--- src/main/java/com/schmuhis/quantumschmuh/TestController.java
+++ src/main/java/com/schmuhis/quantumschmuh/TestController.java
@@ -2,10 +2,11 @@ package com.schmuhis.quantumschmuh;
 
 import com.schmuhis.quantumschmuh.MqttOutputConfiguration.MyGateway;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Random;
 
 @RestController
 @RequiredArgsConstructor
@@ -13,9 +14,32 @@ public class TestController {
 
     private final MyGateway gateway;
 
-    @GetMapping
-    public String test() {
-        gateway.sendToMqtt("from test", new Random().nextBoolean() ? "topic1" : "topic2");
-        return "Hello World";
+    private final DiceService diceService;
+
+    private final PlayerService playerService;
+    private final ControllerService controllerService;
+
+//    @GetMapping
+//    public String test() {
+////        playerService.setHealth(2, 2);
+////        playerService.playSound(1, "schwenglon");
+////        playerService.die(3);
+//        gateway.sendToMqtt("schwenlgon", "die");
+//        gateway.sendToMqtt("schwenlgon", "health");
+//        gateway.sendToMqtt("schwenlgon", "sound");
+//        return "Hello World";
+//    }
+
+    @EventListener(ContextRefreshedEvent.class)
+    public void onStart() {
+        while (true) {
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            controllerService.displayText("INCREDIBILIS");
+        }
     }
 }
